ca90561e238cddaff93b614827d1d2a698c63376
Adenhall <jamesadenhall@gmail.com>
2020-06-11 10:24:06 +0700

    updates

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,19 +1,23 @@
 import React from "react";
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import { useDispatch, useSelector } from "react-redux";
-import {useHistory} from 'react-router-dom'
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useDispatch} from "react-redux";
+import { useHistory } from "react-router-dom";
 
 export default function Login(props) {
-  let history = useHistory()
-  let email = useSelector(state => state.email)
-  let password = useSelector(state => state.password)
+  let history = useHistory();
+  let email = ''
+  let password = ''
   const dispatch = useDispatch();
   const login = (e) => {
     e.preventDefault();
     let user = { email: email, password: password };
-    dispatch({ type: "LOGIN", payload: user });
-    history.goBack();
+    if (email === '' || password === '') {
+      alert("Invalid email or password. Please try again");
+    } else {
+      dispatch({ type: "LOGIN", payload: user });
+      history.goBack();
+    }
   };
 
   return (
@@ -21,7 +25,11 @@ export default function Login(props) {
       <Form>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" onChange={(e) => email = e.target.value} />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => (email = e.target.value)}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -29,7 +37,11 @@ export default function Login(props) {
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" onChange={(e) => password = e.target.value} />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => (password = e.target.value)}
+          />
         </Form.Group>
         <Form.Group controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
